234e6e81497aac5aa8c62c7ddc7c23da3455cf74
Avery Yang <averyyang2674@gmail.com>
2022-05-24 13:58:59 -0500

    add crossOrigin and access-control-allow-origin to header

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //concept: arrays, conditionals, loops, functions
 
-header(audio.crossOrigin = 'anonymous');
-header(Access-Control-Allow-Origin: *);
+header("audio.crossOrigin = 'anonymous'");
+header("Access-Control-Allow-Origin: *");
 
 // audio NOT working on github (development).
 // dev tools said: Failed to load resource: the server responded with a status of 404 ()
